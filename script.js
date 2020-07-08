827245633eaa60d5b6c86cd62a10836ddc2f3538
Smiti Shakya <smitishk12@gmail.com>
2020-07-08 11:34:33 +0545

    added slider in interest section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Hamburger Navigation
 const hamburger = document.getElementById('hamburger');
 const navul = document.getElementById('nav-ul');
 const navbutton = document.getElementById('nav-button');
@@ -6,3 +8,28 @@ hamburger.addEventListener('click', () => {
     navul.classList.toggle('show');
     navbutton.classList.toggle('showbutton');
 });
+//Slide Show for Discover Places of Interest Section
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("discover-slides");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+
